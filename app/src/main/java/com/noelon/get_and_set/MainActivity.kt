58e3b2914b72e1b9bedce2db1816f91851c5ab9d
Noel-elon <noelnwaelugo@gmail.com>
2020-10-20 07:07:33 +0100

    initial commit

---
+++ app/src/main/java/com/noelon/get_and_set/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.noelon.get_and_set
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
