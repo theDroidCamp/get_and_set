f099f106f782cb3a9e1b00d17ef40063223b90e0
Noel-elon <noelnwaelugo@gmail.com>
2020-10-20 07:28:43 +0100

    sample activity done

--- app/src/main/java/com/noelon/get_and_set/MainActivity.kt
+++ app/src/main/java/com/noelon/get_and_set/MainActivity.kt
@@ -2,10 +2,32 @@ package com.noelon.get_and_set
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        displayTextButton.setOnClickListener {
+            val name = name_ET.text.toString()
+            val teamName = TeamNameET.text.toString()
+            val emailAddress = EmailET.text.toString()
+
+            if (name.isNotEmpty() && teamName.isNotEmpty() && emailAddress.isNotEmpty()) {
+
+                textView.text =
+                    "My name is $name, and i'm in team $teamName. Here's my email address too $emailAddress "
+            } else {
+
+                Toast.makeText(this, "Fill in your details please", Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
+
+
     }
 }
